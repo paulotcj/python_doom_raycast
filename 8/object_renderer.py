a000565c9bd17aab3ae0fe374df818182ca3de3d
Paulo Costa <paulotcj@gmail.com>
2023-12-29 20:18:55 -0500

    .

---
+++ 8/object_renderer.py
@@ -0,0 +1,35 @@
+import pygame as pg
+from settings import *
+
+class ObjectRenderer:
+    def __init__(self, game):
+        self.game = game
+        self.screen = game.screen
+        self.wall_textures = self.load_wall_texture()
+
+    def draw(self):
+        self.render_game_objects()
+
+    def render_game_objects(self):
+        list_objects = self.game.ray_casting.objects_to_render
+
+        for depth, image, pos in list_objects:
+            self.screen.blit(image,pos)
+
+
+    @staticmethod
+    def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
+        #The load function returns a Surface object, which is Pygame's term for an image loaded into memory that can be drawn on the screen.
+        #convert_alpha method converts the Surface to a pixel format that includes per-pixel alpha. Alpha is a measure of transparency, 
+        # and per-pixel alpha means that each pixel in the image can have its own level of transparency
+        texture = pg.image.load(path).convert_alpha()
+        return pg.transform.scale(texture, res)
+    
+    def load_wall_texture(self):
+        return {
+            1 : self.get_texture('resources/textures/1.png'),
+            2 : self.get_texture('resources/textures/2.png'),
+            3 : self.get_texture('resources/textures/3.png'),
+            4 : self.get_texture('resources/textures/4.png'),
+            5 : self.get_texture('resources/textures/5.png'),
+        }
---
+++ 8/player.py
@@ -0,0 +1,78 @@
+from settings import *
+import pygame as pg
+import math
+
+class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
+
+    def movement(self):
+        #-----
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        dx, dy = 0, 0
+        speed = PLAYER_SPEED * self.game.delta_time
+        speed_sin = speed * sin_a
+        speed_cos = speed * cos_a
+        #-----
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w]:
+            dx += speed_cos
+            dy += speed_sin
+        if keys[pg.K_s]:
+            dx += -speed_cos
+            dy += -speed_sin
+        if keys[pg.K_a]:
+            dx += speed_sin
+            dy += -speed_cos
+        if keys[pg.K_d]:
+            dx += -speed_sin
+            dy += speed_cos
+        #-----
+            
+        self.check_wall_collision(dx, dy)
+
+
+        if keys[pg.K_LEFT]: #arrow keys (left)
+            self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
+        if keys[pg.K_RIGHT]:
+            self.angle += PLAYER_ROT_SPEED * self.game.delta_time
+        
+        # ensure that self.angle stays within the range of 0 to math.tau (0 to 2π), effectively keeping the angle within a full circle
+        self.angle %= math.tau # tau is a mathematical constant equal to 2π
+
+    def check_wall(self, x, y):
+        #all the spaces where the player can move are set to false, so if the player enter a space that returns true, then it must be
+        #  a wall and therefore the player must be stopped
+        return (x, y) not in self.game.map.world_map
+
+    def check_wall_collision(self, dx, dy):
+        scale = PLAYER_SIZE_SCALE / self.game.delta_time
+
+        if self.check_wall(int(self.x + dx * scale), int(self.y)):
+            self.x += dx
+        if self.check_wall(int(self.x), int(self.y + dy  * scale)):
+            self.y += dy
+        
+
+
+    def draw(self):
+        # pg.draw.line( self.game.screen, 'blue', 
+        #              ( self.x * 100, self.y * 100),
+        #              ( self.x * 100 + WIDTH * math.cos(self.angle), 
+        #                self.y * 100 + WIDTH * math.sin(self.angle)  ), 
+        #              2 )                  
+        
+        pg.draw.circle(self.game.screen, 'green' , (self.x * 100, self.y * 100) , 15)
+
+    def update(self):
+        self.movement()
+
+    @property
+    def pos(self):
+        return self.x, self.y
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
